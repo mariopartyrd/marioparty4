0de51f3ffaf5ab5df4edffa49588816a4a5999da
dbalatoni13 <stubiflex.junior@gmail.com>
2025-01-12 09:25:08 +0100

    Import TRK 2.6

---
+++ include/PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/stdio.h
@@ -0,0 +1,13 @@
+#ifndef MSL_STDIO_H_
+#define MSL_STDIO_H_
+
+#include "char_io.h" // IWYU pragma: export
+#include "file_io.h" // IWYU pragma: export
+#include "printf.h"  // IWYU pragma: export
+#include "extras.h"  // IWYU pragma: export
+
+#define stdin  (&__files._stdin)
+#define stdout (&__files._stdout)
+#define stderr (&__files._stderr)
+
+#endif
