6039603ce879b6b54a653205bc186929eee47e39
j-ac <83185117+j-ac@users.noreply.github.com>
2025-05-31 18:30:56 -0400

    revert changes to mtx.h

--- include/dolphin/mtx.h
+++ include/dolphin/mtx.h
@@ -191,13 +191,13 @@ void C_VECHalfAngle(const Vec* a, const Vec* b, Vec* half);
 
 #ifdef GEKKO
 void PSVECAdd(const Vec* a, const Vec* b, Vec* ab);
-void PSVECSubtract(const Vec* a, const Vec* b, Vec* result);
+void PSVECSubtract(const Vec* a, const Vec* b, Vec* a_b);
 void PSVECScale(const Vec* src, Vec* dst, f32 scale);
 void PSVECNormalize(const Vec* src, Vec* unit);
 f32 PSVECSquareMag(const Vec* v);
 f32 PSVECMag(const Vec* v);
 f32 PSVECDotProduct(const Vec* a, const Vec* b);
-void PSVECCrossProduct(const Vec* a, const Vec* b, Vec* result);
+void PSVECCrossProduct(const Vec* a, const Vec* b, Vec* axb);
 f32 PSVECSquareDistance(const Vec* a, const Vec* b);
 f32 PSVECDistance(const Vec* a, const Vec* b);
 #endif
